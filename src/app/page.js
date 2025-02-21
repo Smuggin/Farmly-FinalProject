6cd2cb50e4c7897f80b914bb8b6a5bb13e603c51
Natum Tongiyang <95202148+Smuggin@users.noreply.github.com>
2025-02-21 13:53:35 +0700

    Feature: Adding Navbar component to main menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
-import Navbar from "../components/Navbar";
-
+import Navbar from "@/components/nav";
 export default function Home() {
   return (
+    <>
+    <Navbar />
     <div>
     </div>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -13,5 +15,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 };
